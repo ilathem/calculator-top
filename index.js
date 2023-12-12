8836e8935afec5f2cd07fe54a9d4a49e6eb6b041
ilathem <isaiahlathem@gmail.com>
2023-12-12 15:29:29 -0600

    Refactoring and decimals
    - Refactored event listener block to multiple methods for better
      readibility
    - Added support for decimal numbers

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const calculatorLogic = {
     '+': (a, b) => Number(a) + Number(b),
     '-': (a, b) => Number(a) - Number(b),
-    '*': (a, b) => Number(a) * Number(b),
-    '/': (a, b) => Number(a) / Number(b),
+    '×': (a, b) => Number(a) * Number(b),
+    '÷': (a, b) => Number(a) / Number(b),
 }
 
 function operate(arrInput) {
     const precedence = [
-        ['*', "/"],
+        ['×', "÷"],
         ["+", "-"],
     ]
     for (const operators of precedence) {
@@ -19,7 +19,6 @@ function operate(arrInput) {
             }
         }
     }
-    console.log(arrInput[0].split("."))
     if (arrInput[0].split(".")[1] && arrInput[0].split(".")[1].length > 3) {
         return Number(arrInput[0]).toFixed(3);
     } else {
@@ -29,60 +28,76 @@ function operate(arrInput) {
 
 const displayText = document.querySelector('#text');
 const displayTextContainer = document.querySelector(".text-container");
-
 const buttons = document.querySelectorAll(".calc-button")
-
 const computeArray = [];
 let currentOperand = '';
 
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        if (displayText.innerText === 'Calculator') {
-            displayText.innerText = '';
-        }
-        if (event.target.innerText === 'Clear') {
-            displayText.innerText = 'Calculator';
-            computeArray.splice(0, computeArray.length);
-            currentOperand = '';
-            console.clear();
-            displayTextContainer.classList.remove('solved');
-        } else if (event.target.innerText === '=') {
-            if (computeArray.length === 0) {
-                displayText.innerText = 'Calculator';
-                return;
-            }
-            if (currentOperand) {
-                computeArray.push(currentOperand);
-                currentOperand = '';
-            } else {
-                computeArray.pop();
-            }
-            currentOperand = '';
-            displayText.innerText = operate(computeArray);
-            computeArray.splice(0, computeArray.length);
-            displayTextContainer.classList.add('solved');
-            if (displayText.innerText === 'Infinity') {
-                displayText.innerText = 'You fool!';
-            }
-        } else if (
-            ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-            .includes(event.target.innerText)) 
-        {
-            if (displayTextContainer.classList.contains('solved')) {
-                displayText.innerText = '';
-                displayTextContainer.classList.remove('solved')
-            }
-            displayText.innerText += button.innerText;
-            currentOperand += button.innerText;
-        } else {
-            if (currentOperand === '') {
-                displayText.innerText = 'Calculator';
-                return;
-            }
-            displayText.innerText += button.innerText;
-            computeArray.push(currentOperand);
-            computeArray.push(button.innerText);
-            currentOperand = '';
-        }
-    })
-})
+function clearCalculator() {
+    displayText.innerText = 'Calculator';
+    computeArray.splice(0, computeArray.length);
+    currentOperand = '';
+    console.clear();
+    displayTextContainer.classList.remove('solved');
+}
+
+function computeAnswer() {
+    if (computeArray.length === 0) {
+        displayText.innerText = 'Calculator';
+        return;
+    }
+    if (currentOperand) {
+        computeArray.push(currentOperand);
+        currentOperand = '';
+    } else {
+        computeArray.pop();
+    }
+    currentOperand = '';
+    displayText.innerText = operate(computeArray);
+    computeArray.splice(0, computeArray.length);
+    displayTextContainer.classList.add('solved');
+    if (displayText.innerText === 'Infinity') {
+        displayText.innerText = 'You fool!';
+    }
+}
+
+function addNumber(button) {
+    if (displayTextContainer.classList.contains('solved')) {
+        displayText.innerText = '';
+        displayTextContainer.classList.remove('solved')
+    }
+    displayText.innerText += button.innerText;
+    currentOperand += button.innerText;
+}
+
+function addOperator(button) {
+    if (currentOperand === '') {
+        displayText.innerText = 'Calculator';
+        return;
+    }
+    displayText.innerText += button.innerText;
+    computeArray.push(currentOperand);
+    computeArray.push(button.innerText);
+    currentOperand = '';
+}
+
+function handleBtnClick(event) {
+    if (displayText.innerText === 'Calculator') {
+        displayText.innerText = '';
+    }
+    if (event.target.innerText === 'C') {
+        clearCalculator();
+    } else if (event.target.innerText === '=') {
+        computeAnswer();
+    } else if (
+        ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        .includes(event.target.innerText)) 
+    {
+        addNumber(event.target);
+    } else {
+        addOperator(event.target);
+    }
+}
+
+for(const button of buttons) {
+    button.addEventListener('click', (event) => handleBtnClick(event));
+}

da72c9eefe14d8de26017eb2f26f02f9162eabd2
ilathem <isaiahlathem@gmail.com>
2023-12-11 21:47:54 -0600

    QoL Improvements
    - After a successful calculation, the border will change to green
    - After a successful calculation, pressing a button will reset the
      field.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function operate(arrInput) {
         ["+", "-"],
     ]
     for (const operators of precedence) {
-        console.log(arrInput);
         for (let index = 0; index < arrInput.length; index++) {
             if (operators.includes(arrInput[index])) {
                 const result = calculatorLogic[arrInput[index]](arrInput[index - 1], arrInput[index + 1]);
@@ -24,6 +23,7 @@ function operate(arrInput) {
 }
 
 const displayText = document.querySelector('#text');
+const displayTextContainer = document.querySelector(".text-container");
 
 const buttons = document.querySelectorAll(".calc-button")
 
@@ -40,6 +40,7 @@ buttons.forEach(button => {
             computeArray.splice(0, computeArray.length);
             currentOperand = '';
             console.clear();
+            displayTextContainer.classList.remove('solved');
         } else if (event.target.innerText === '=') {
             computeArray.push(currentOperand);
             currentOperand = '';
@@ -48,10 +49,15 @@ buttons.forEach(button => {
             }
             displayText.innerText = operate(computeArray);
             computeArray.splice(0, computeArray.length);
+            displayTextContainer.classList.add('solved');
         } else if (
             ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
             .includes(event.target.innerText)) 
         {
+            if (displayTextContainer.classList.contains('solved')) {
+                displayText.innerText = '';
+                displayTextContainer.classList.remove('solved')
+            }
             displayText.innerText += button.innerText;
             currentOperand += button.innerText;
         } else {
